0edae402cc8c71a54fd3615f85c1806b4303a8df
karina1208 <56858132+karina1208@users.noreply.github.com>
2020-08-12 19:45:37 +0800

    Add new game

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ board = [
     ]
 
 #initial page
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['POST'])
 def index():
     global board
     #reset board each time
@@ -40,7 +40,7 @@ def index():
     return render_template('index.html', playBoard=board)
 
 #get and return jSON data of cell
-@app.route('/checkvalid', methods=['POST', 'GET'])
+@app.route('/checkvalid', methods=['GET'])
 def checkvalid():
     position = request.args.get('position', 0, type=int)
     number = request.args.get('number', 0, type=int)
